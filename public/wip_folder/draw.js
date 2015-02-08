be99610e73f7204542c03d2d14c6aecb07ee8f21
berwin7996 <berwin7996@gmail.com>
2015-02-08 13:13:23 -0600

    drawing will now elicit a musical melody based on a white-space removed signature that is columned off and means based on the coords in that section. The note is calculated by sectioning off that column into rows and then storing into an output note array. Note length is still random, however. it is a @todo

---
+++ public/wip_folder/draw.js
@@ -0,0 +1,190 @@
+
+// Copyright 2010 William Malone (www.williammalone.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var canvas;
+var context;
+var canvasWidth = 490;
+var canvasHeight = 220;
+var padding = 25;
+var lineWidth = 8;
+var colorPurple = "#cb3594";
+var colorGreen = "#659b41";
+var colorYellow = "#ffcf33";
+var colorBrown = "#986928";
+var clickX = new Array();
+var clickY = new Array();
+var clickColor = new Array();
+var clickTool = new Array();
+var clickSize = new Array();
+var clickDrag = new Array();
+var paint = false;
+var curColor = colorPurple;
+var curTool = "marker";
+var curSize = "normal";
+var mediumStartX = 18;
+var mediumStartY = 19;
+var mediumImageWidth = 93;
+var mediumImageHeight = 46;
+var toolHotspotStartY = 23;
+var toolHotspotHeight = 38;
+var sizeHotspotStartY = 157;
+var sizeHotspotHeight = 36;
+var sizeHotspotWidthObject = new Object();
+sizeHotspotWidthObject.huge = 39;
+sizeHotspotWidthObject.large = 25;
+sizeHotspotWidthObject.normal = 18;
+sizeHotspotWidthObject.small = 16;
+var totalLoadResources = 8;
+var curLoadResNum = 0;
+/**
+* Calls the redraw function after all neccessary resources are loaded.
+*/
+function resourceLoaded()
+{
+	//if(++curLoadResNum >= totalLoadResources){
+		redraw();
+	//}
+}
+
+/**
+* Creates a canvas element, loads images, adds events, and draws the canvas for the first time.
+*/
+function prepareCanvas()
+{
+	// Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
+	var canvasDiv = document.getElementById('canvasDiv');
+	canvas = document.createElement('canvas');
+	canvas.setAttribute('width', canvasWidth);
+	canvas.setAttribute('height', canvasHeight);
+	canvas.setAttribute('id', 'canvas');
+	canvas.setAttribute('style', 'border:1px solid #000000;')
+	canvasDiv.appendChild(canvas);
+	if(typeof G_vmlCanvasManager != 'undefined') {
+		canvas = G_vmlCanvasManager.initElement(canvas);
+	}
+	context = canvas.getContext("2d"); // Grab the 2d canvas context
+	// Note: The above code is a workaround for IE 8 and lower. Otherwise we could have used:
+	//     context = document.getElementById('canvas').getContext("2d");
+	
+
+	// Add mouse events
+	// ----------------
+	$('#canvas').mousedown(function(e)
+	{
+		// Mouse down location
+		var mouseX = e.pageX - this.offsetLeft;
+		var mouseY = e.pageY - this.offsetTop;
+		$('#test').text(coords.length + ' mousex:' + mouseX + ' mousey:' + mouseY);
+		coords.push({x: mouseX, y: mouseY});
+
+		paint = true;
+		addClick(mouseX, mouseY, false);
+		redraw();
+	});
+	
+	$('#canvas').mousemove(function(e){
+		if(paint==true){
+			var mouseX = e.pageX - this.offsetLeft;
+			var mouseY = e.pageY - this.offsetTop;
+			addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+			$('#test').text(coords.length + ' mousex:' + mouseX + ' mousey:' + mouseY);
+			coords.push({x: mouseX, y: mouseY});
+			redraw();
+		}
+	});
+	
+	$('#canvas').mouseup(function(e){
+		paint = false;
+	  	redraw();
+	});
+	
+	$('#canvas').mouseleave(function(e){
+		paint = false;
+	});
+}
+
+/**
+* Adds a point to the drawing array.
+* @param x
+* @param y
+* @param dragging
+*/
+function addClick(x, y, dragging)
+{
+	clickX.push(x);
+	clickY.push(y);
+	clickTool.push(curTool);
+	clickColor.push(curColor);
+	clickSize.push(curSize);
+	clickDrag.push(dragging);
+}
+
+/**
+* Clears the canvas.
+*/
+function clearCanvas()
+{
+	context.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+/**
+* Redraws the canvas.
+*/
+function redraw()
+{
+	// parse what's on the canvas atm
+	//parseCanvas();
+	
+	
+	clearCanvas();
+	
+	var radius;
+	var i = 0;
+	for(; i < clickX.length; i++)
+	{		
+		if(clickSize[i] == "small"){
+			radius = 2;
+		}else if(clickSize[i] == "normal"){
+			radius = 5;
+		}else if(clickSize[i] == "large"){
+			radius = 10;
+		}else if(clickSize[i] == "huge"){
+			radius = 20;
+		}else{
+			alert("Error: Radius is zero for click " + i);
+			radius = 0;	
+		}
+		
+		context.beginPath();
+		if(clickDrag[i] && i){
+			context.moveTo(clickX[i-1], clickY[i-1]);
+		}else{
+			context.moveTo(clickX[i], clickY[i]);
+		}
+		context.lineTo(clickX[i], clickY[i]);
+
+		context.closePath();
+		
+		context.lineJoin = "round";
+		context.lineWidth = radius;
+		context.stroke();
+		
+	}
+	context.restore();
+	
+}
+
+
+/**/
