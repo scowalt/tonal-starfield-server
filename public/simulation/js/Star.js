bc0efcf1c434365886a23073481fb8c8fb439255
Scott Walters <srwalte2@illinois.edu>
2015-02-14 19:56:08 -0600

    Added physics engine
    Also, stars move not the camera

--- public/simulation/js/Star.js
+++ public/simulation/js/Star.js
@@ -1,5 +1,6 @@
 // object
 var Star = function(position, color){
+	// visuals
 	var material = new THREE.MeshBasicMaterial({
 		color: 0xffffff
 	});
@@ -7,10 +8,29 @@ var Star = function(position, color){
 	mesh.position.set(position.x, position.y, position.z);
 	mesh.material.color.setRGB(color.red, color.blue, color.green);
 
+	// physics
+	var sphereBody = new CANNON.Body({
+		mass: 5,
+		position: new CANNON.Vec3(position.x, position.y, position.z),
+		shape: new CANNON.Sphere(Star.radius),
+		velocity: Star.velocity
+	});
+
 	this.getMesh = function(){
 		return mesh;
+	};
+
+	this.getBody = function () {
+		return sphereBody;
+	};
+
+	this.updatePosition = function() {
+		mesh.position.copy(sphereBody.position);
+		mesh.quaternion.copy(sphereBody.quaternion);
 	};
 };
 
 // static variables
-Star.geometry =  new THREE.SphereGeometry(2, 12, 12);
+Star.radius = 2;
+Star.geometry = new THREE.SphereGeometry(Star.radius, 12, 12);
+Star.velocity = new CANNON.Vec3(0,0,500);
