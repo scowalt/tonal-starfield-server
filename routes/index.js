aeb484f7e225b242c893cc1e76aaa4381dbb3738
Scott Walters <srwalte2@illinois.edu>
2015-03-11 00:13:31 -0500

    Root redirects to simulation

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ module.exports = function(io) {
 	var express = require('express');
 	var router = express.Router();
 
+	router.get('/', function (req, res){
+		res.redirect('/simulation');
+	});
+
 	router.post('/client/submit', function (req, res) {
 		io.emit('comet', req.body);
 		res.status(200).send('ok');
